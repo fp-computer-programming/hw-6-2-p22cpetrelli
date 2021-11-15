9272f1e1a9d20130268514ab835c213f79445ec5
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-11-15 09:50:26 -0500

    add file

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: CJP (AMDG) 11/15/21
+
+# Question 1
+lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(lst))
+
+# Question 3
+print(lst[2])
+
+# Question 4
+print(5 not in lst)
+
+# Question 5
+print(10 not in lst)
+
+# Question 6
+newlst = ["Cam", "Dan", "Jack", "James"]
+
+# Question 7
+print(newlst[3])
+
+# Question 8
+print(newlst * 3)
+
+# Question 9
+print(lst + newlst)
+
+# Question 10
+print(len(newlst))
